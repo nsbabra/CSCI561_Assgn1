2a39af9e2ce2cc8f1319c38a91f8c25e2e81cde0
Navneet Singh Babra <nsbabra@gmail.com>
2015-09-23 06:24:17 -0700

    dfs and bfs, with documentation tested with test cases. Fixed the Path finder and improved performance and edge cases.

--- CSCIAssign1/src/waterFlow.java
+++ CSCIAssign1/src/waterFlow.java
@@ -1,11 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
@@ -20,7 +14,6 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.LinkedList;
-import java.util.Queue;
 import java.util.Stack;
 import java.util.StringTokenizer;
 
@@ -29,37 +22,62 @@ import java.util.StringTokenizer;
  * @author nsbabra
  */
 public class waterFlow {
-    private static int TEST_CASES ;
-    private static HashMap<String, Node> mGraph ;
+//    private static int TEST_CASES ;
     
+    //Variables used for Current Test Case
     private static Node mStartNode ;
     private static HashMap<String, Node> mDestinationNodes;
     private static HashMap<String, Node> mMiddleNodes;
     private static int mStartTime;
     
+    
+    //Because Macs and Windows have different newline character.
     private static final String NEWLINE = System.getProperty("line.separator");
 
+    //Test Cases in the Input File
     private static Test[] tests;
+    
 
     public static void main(String[] args) throws FileNotFoundException {
         System.out.println(Arrays.toString(args));
+        
+        // If output.txt exists, delete it. Just a sanity check.
+        File f = new File("output.txt");
+        if(f.exists()){
+            f.delete();
+        }
+        
+        //Runtime Arguments Parsing
         if(args.length!=2){
-            System.out.println("No/Bad File Specified");
+             try (FileWriter filewriter = new FileWriter("output.txt")) {
+                    filewriter.write("Exception in Parsing th File");
+                    filewriter.close();
+             }catch(IOException ex){
+                 System.out.println("Exception in Writing to File : "+ex);
+             }
             System.exit(1); 
         }
         if(!args[0].equals("-i") || !args[1].endsWith(".txt")){
-            System.out.println("No/Bad File Specified 2");
+             try (FileWriter filewriter = new FileWriter("output.txt")) {
+                    filewriter.write("Exception in Parsing th File");
+                    filewriter.close();
+             }catch(IOException ex){
+                 System.out.println("Exception in Writing to File : "+ex);
+             }
             System.exit(1); 
         }
 
+        //Reading from the File
         BufferedReader reader = new BufferedReader(new FileReader(args[1]));
         try{
-            TEST_CASES = Integer.parseInt(reader.readLine());
+            int _testCases = Integer.parseInt(reader.readLine());
 
-            //Number of Tests 
-            tests =  new Test[TEST_CASES];
+            //Instantiate the Test array 
+            tests =  new Test[_testCases];
 
-            for(int i=0; i<TEST_CASES ; i++){
+            
+            //Reads the Input file and creates "test" objects for each test
+            for(int i=0; i<_testCases ; i++){
 
                 String type = reader.readLine();
                 String startNode = reader.readLine();
@@ -75,19 +93,25 @@ public class waterFlow {
 
                 int startTime = Integer.parseInt(reader.readLine());
 
-                tests[i] = new Test(type, startNode, destinations, middleNodes, pipes, graph, startTime);
+                tests[i] = new Test(type, startNode, destinations, middleNodes, graph, startTime);
                 reader.readLine(); //Empty Line
             }
         
         }catch(IOException | NumberFormatException ex){
-        	try{
-        		reader.close();
-        	}catch(Exception e){	
-        	}
-            System.out.println("Exception in Parsing File : "+ex);
+            try (FileWriter filewriter = new FileWriter("output.txt")) {
+                reader.close();
+                filewriter.write("Exception in Parsing the Input File");
+                filewriter.close();
+            }catch(Exception e){
+                e.printStackTrace();
+            }
+            System.out.println("Exception in Parsing Input File : "+ex);
+            System.exit(1);
         }
         
         try (FileWriter filewriter = new FileWriter("output.txt")) {
+            
+            //Iterate on all the Tests 
             for (Test currentTest : tests) {
                 switch(currentTest.type){
                     case "BFS":
@@ -97,167 +121,266 @@ public class waterFlow {
                     case "DFS":
                         BFSDFSGraphBuilder(currentTest);
                         DFS(filewriter);
-    //                    case "UCS":
-    //                        new UCS(currentTest, filewriter);
+                        break;
+                    case "UCS":
+                        UCS(filewriter);
+                        break;
                 }
             }
         } catch (IOException ex) {
             Logger.getLogger(waterFlow.class.getName()).log(Level.SEVERE, null, ex);
         }
-       
-        
-        
-        
-        
-
+      
     }
-    
-    
+       
     private static void BFSDFSGraphBuilder(Test currentTest){
-        mGraph = new HashMap<>();
+
+        HashMap<String, Node> graph = new HashMap<>();
+        
         mStartNode = new Node(currentTest.startNode);
         mStartTime = currentTest.startTime;
         
-        mGraph.put(mStartNode.label, mStartNode);
+        graph.put(mStartNode.label, mStartNode);
         
         mDestinationNodes = new HashMap<>();
+        
+        // Adding Goal Nodes to the HashMap
         StringTokenizer tokenizerDest = new StringTokenizer(currentTest.destinationNodes);
         while(tokenizerDest.hasMoreTokens()){
             String currentToken = tokenizerDest.nextToken();
             Node destNode = new Node(currentToken);
             mDestinationNodes.put(currentToken, destNode);
-            mGraph.put(currentToken, destNode);
+            graph.put(currentToken, destNode);
         }
         
-//        mMiddleNodes = new HashMap<>();
+        //Adds middle nodes to graph, which will be used in adjancey list creator.
         StringTokenizer tokenizerMiddle = new StringTokenizer(currentTest.middleNodes);
         while(tokenizerMiddle.hasMoreTokens()){
             String currentToken = tokenizerMiddle.nextToken();
-            
             Node middleNode = new Node(currentToken);
-            
-//            mMiddleNodes.put(currentToken, middleNode);
-            mGraph.put(currentToken, middleNode);
+            graph.put(currentToken, middleNode);
         }
         
+        //Adjancy List Settter, for every node
         StringTokenizer tokenizer = null;
         for (String graphLine : currentTest.graph) {
             tokenizer = new StringTokenizer(graphLine);
-            Node node = mGraph.get(tokenizer.nextToken());
-            node.adjList.add(mGraph.get(tokenizer.nextToken()));
+            Node node = graph.get(tokenizer.nextToken());
+            node.adjList.add(graph.get(tokenizer.nextToken()));
         }
     }
 
     
     private static void BFS(FileWriter fileWriter){
+                 
+         //Start Node is the Goal, So write to file and return. 
+         //Should not happen. but here because of sanity check
+         if(mDestinationNodes.containsKey(mStartNode.label)){
+             try{
+                fileWriter.append((mDestinationNodes.get(mStartNode.label)).toString());
+                fileWriter.append(" "+(mStartTime%24 == 0 ? 0: mStartTime)+NEWLINE);
+             }
+             catch(IOException ex){
+                 System.out.println("Exception in writing to File : "+ex);
+             }
+             return;
+         }
          
-         Queue<Node> fifoQueue = new LinkedList<>();
+         //Frontier, initialized with start ode
+         LinkedList<Node> fifoQueue = new LinkedList<>();
          fifoQueue.add(mStartNode);
          
-         Queue<Node> exploredQueue = new LinkedList<>();
+         //Explored Queue (Empty)
+//         LinkedList<Node> exploredQueue = new LinkedList<>();
          
          
          while(true){
+             //If Frontier is empty, return None in File  
              if(fifoQueue.isEmpty()){
-                 System.out.println("Queue Empty in BFS");
-                 break;
+                 try{
+                    fileWriter.append("None"+NEWLINE);
+                 }
+                 catch(IOException ex){
+                     ex.printStackTrace();
+                 }
+                 return;
              }
-             
+            
+             //If Not, then remove the First Node (is queue.remove) from Frontier 
              Node currentNode = (Node) fifoQueue.remove();
-             currentNode.state = 1;
-             exploredQueue.add(currentNode);
              
+             //Set State to 1 - i.e. Explored
+             currentNode.state = 1;
+             
+             //Add to Explored Queue
+//             exploredQueue.add(currentNode);
+             
+             //Comparator for adjList, Alphabetical Sorting of adjList before inserting in Frontier
              Comparator<Node> nodeComparator = (Node o1, Node o2) -> o1.toString().compareTo(o2.toString());
              
-             Collections.sort(currentNode.adjList, nodeComparator );
+             //Sort them by Alphabetical Order
+             Collections.sort(currentNode.adjList, nodeComparator);
+             
+             //For all the nodes in current node's adj list if the node is not explored or in frontier
+             //Check if it's Destination. if yes, write to File and Return.
+             //Else add the node to frontier
              for(Node node: currentNode.adjList){
+                 //If Node is NOT explored and it is not in Frontier (fifoQueue)
                  if(node.state==0 && !fifoQueue.contains(node)){
+                    node.parent = currentNode;
+
+                     //If the node is Destination
                      if(mDestinationNodes.containsKey(node.label)){
-                          //FileWrite and RETURN;
-                         //TODO : Remove inner path loop, only for debugging
-                         System.out.println("Path is : ");
-                         int i = 0;
-                         while(!exploredQueue.isEmpty()){
-                        	 i++;
-                            System.out.print((exploredQueue.remove()).toString());
+                         int i=0;
+                         
+                         Node tempforPath = mDestinationNodes.get(node.label);
+                         
+                         if(tempforPath!=null){
+                         while(!mStartNode.equals(tempforPath)){
+                             i++;
+//                             System.out.println("Path backwards : "+tempforPath.label);
+                             tempforPath = tempforPath.parent;
+                             if(tempforPath==null){
+                                 break;
+                             }
                          }
-                         System.out.println(mDestinationNodes.get(node.label));
+                         }
+
                          try{
                             fileWriter.append((mDestinationNodes.get(node.label)).toString());
-                            fileWriter.append(" "+(mStartTime+i)+NEWLINE);
+                            int cost = (mStartTime+i);
+                            while(cost>=24){
+                                cost = cost - 24;
+                            }
+                            fileWriter.append(" "+(cost)+NEWLINE);
                          }
                          catch(IOException ex){
                              System.out.println("Exception in writing to File : "+ex);
                          }
-                         System.out.println("Cost is : "+(mStartTime+i));
+//                         System.out.println("Cost is : "+(mStartTime+i));
                          return;
                      }
+                     //If node is not destination, just add to frontier
                      fifoQueue.add(node);
                  }
              }
         }
-        //Labha hi nahi.. So nothing to write ?
     }
     
     private static void DFS(FileWriter fileWriter){
+        
+         //Start Node is the Goal, So write to file and return. 
+         //Should not happen. but here because of sanity check
+         if(mDestinationNodes.containsKey(mStartNode.label)){
+             try{
+                fileWriter.append((mDestinationNodes.get(mStartNode.label)).toString());
+                fileWriter.append(" "+(mStartTime%24 == 0 ? 0: mStartTime)+NEWLINE);
+             }
+             catch(IOException ex){
+                 System.out.println("Exception in writing to File : "+ex);
+             }
+             return;
+         }
+         
+         
+         //Stack for DFS
          Stack<Node> lifoStack = new Stack<>();
-         lifoStack.push(mStartNode);
-         Queue<Node> exploredQueue = new LinkedList<>();     
+         lifoStack.add(mStartNode);
+         
+         //Loop to iterate over the stack, while it's empty
          while(true){
              if(lifoStack.isEmpty()){
-                 System.out.println("Stack Empty in DFS");
+                 //If Stack is Empty, that means Goal Node not found, so write None and return. 
+                 try{
+                    fileWriter.append("None"+NEWLINE);
+                 }
+                 catch(Exception ex){
+                     ex.printStackTrace();
+                 }
                  break;
              }
              
+             //Remove the last node from the List. ie. LIFO ie. Stack.pop()
              Node currentNode = lifoStack.pop();
-             currentNode.state = 1;
-             exploredQueue.add(currentNode);
              
+             //Set the state to be 1 (1 = visited)
+             currentNode.state = 1;
+             
+             //Comapator for Alphabetical Sorting
              Comparator<Node> nodeComparator = (Node o1, Node o2) -> o1.toString().compareTo(o2.toString());
              
+             //Sort the adj list of currentNode, so that stack pushes are in Alphabetical order
              Collections.sort(currentNode.adjList, nodeComparator);
              
-             for(Node node: currentNode.adjList){
+           
+             //Iterate over Adj list (already Sorted) to check if it contains Goal Node or not 
+             //If Yes, Calculate path and write to file.
+             //Else, push the node on the stack.
+             for (Node node : currentNode.adjList) {
                  if(node.state==0 && !lifoStack.contains(node)){
+                     
+                     //Set the parent , used to trace the path back (shortest path)
+                     node.parent = currentNode;
+                    
+                     //If node (in adj list) is one of the Goal Nodes
                      if(mDestinationNodes.containsKey(node.label)){
-                          //FileWrite and RETURN;
-                         //TODO : Remove inner path loop, only for debugging
-                         System.out.println("Path is : ");
-                         int i = 0;
-                         while(!exploredQueue.isEmpty()){
-                        	 i++;
-                            System.out.print((exploredQueue.remove()).toString());
+                         //Goal Node Found
+                         //Find the Path weight by going upwards
+                         int i=0;  
+                         Node tempforPath = mDestinationNodes.get(node.label);                       
+                         if(tempforPath!=null){
+                             while(!mStartNode.equals(tempforPath)){
+                                 i++;
+                                 tempforPath = tempforPath.parent;
+                                 if(tempforPath==null){
+                                     break;
+                                 }
+                             }
                          }
-                         System.out.println(mDestinationNodes.get(node.label));
+                         
+                         //Writes Goal Node to the file 
+                         //Parses the Path Weight and writes that too
                          try{
-                            fileWriter.append((mDestinationNodes.get(node.label)).toString());
-                            fileWriter.append(" "+(mStartTime+i)+NEWLINE);
-                         }
-                         catch(IOException ex){
+                             fileWriter.append((mDestinationNodes.get(node.label)).toString());
+                             int cost = (mStartTime+i);
+                             while(cost>=24){
+                                 cost = cost - 24;
+                             }
+                             fileWriter.append(" "+(cost)+NEWLINE);
+                         } catch(IOException ex){
                              System.out.println("Exception in writing to File : "+ex);
                          }
-                         System.out.println("Cost is : "+(mStartTime+i));
                          return;
                      }
-                     else{
-                        lifoStack.push(node);
-                     }
+                     //If Not a Destination node, push on the Stack
+                     lifoStack.push(node);    
                  }
              }
         }
-        //Labha hi nahi.. So nothing to write ?
     }
     
-       
+    private static void UCS(FileWriter fileWriter){
+        try{
+        fileWriter.append("None"+NEWLINE);
+        }
+        catch(Exception ex){
+            ex.printStackTrace();;
+        }
+        
+    }
+    
     private static class Node{
         String label;
         List<Node> adjList;
         int state ;
+        Node parent;
         
         public Node(String label){
             this.label = label;
             adjList = new ArrayList<>();
             state = 0;
+            parent = null;
+                   
         }
 
         @Override
@@ -266,16 +389,20 @@ public class waterFlow {
         }
     }
       
+    /**
+     * Used to read objects from the Input File. 
+     * Doesn't represent any ACTUAL object.
+     */
     private static class Test{
-        String type;
-        String startNode;
-        String destinationNodes;
-        String middleNodes;
-        String[] graph;
-        int startTime;
+        String type;  //First Line of Case
+        String startNode;  //Second
+        String destinationNodes; //Third
+        String middleNodes; //Fourth
+        String[] graph; //Number of Lines followed by line num 5 == the count given in line 5
+        int startTime;  //Last Line
 
         public Test(String type, String startNode, String destinationNodes, String middleNodes, 
-                                        int numPipes, String[] graph, int startTime) {
+                                         String[] graph, int startTime) {
             this.type = type;
             this.startNode = startNode;
             this.destinationNodes = destinationNodes;
